2ade5763bc66c52e067c825c0c555258dadb7eed
zhaochuanzhen <zhaochuanzhen@jd.com>
2022-04-30 20:03:43 +0800

    数组

---
+++ src/main/java/com/mright/algo/array/Array.java
@@ -0,0 +1,105 @@
+package com.mright.algo.array;
+
+import java.text.MessageFormat;
+
+/**
+ * int数组相关操作
+ *
+ * @author zhaochuanzhen
+ * @date 2022/4/24 10:51 上午
+ * @since 1.0
+ */
+public class Array {
+
+    /**
+     * 实际存储数据的数组，数组长度为n，里边存储了count个数据
+     */
+    private int[] data;
+
+    /**
+     * 数组长度
+     */
+    private int n;
+
+    /**
+     * 实际存放的数据个数 0 <= count <= n
+     */
+    private int count;
+
+    /**
+     * 构造器，定义初始值
+     *
+     * @param capacity 初始数组大小
+     */
+    public Array(int capacity) {
+        this.data = new int[capacity];
+        this.n = capacity;
+        this.count = 0;
+    }
+
+    /**
+     * 数组的随机访问，如果请求下标不在范围内，那么默认返回-1
+     *
+     * @param index 访问下标
+     * @return 实际数据
+     */
+    public int find(int index) {
+        if (index < 0 || index > count) {
+            throw new IndexOutOfBoundsException(MessageFormat.format("数组大小为【】，index为【】，数组越界", this.count, index));
+        }
+        return this.data[index];
+    }
+
+    public boolean insert(int index, int value) {
+        if (count == n) {
+            throw new IndexOutOfBoundsException(MessageFormat.format("数组大小为【】，容量为【】，数组已满，无法再插入", this.count, this.n));
+        }
+
+        if (index < 0 || index > count) {
+            throw new IndexOutOfBoundsException(MessageFormat.format("数组大小为【】，index为【】，数组越界", this.count, index));
+        }
+
+        System.arraycopy(data, index, data, index + 1, count - index);
+
+        data[index] = value;
+        count++;
+        return true;
+    }
+
+    public boolean delete(int index) {
+        if (index < 0 || index > count) {
+            throw new IndexOutOfBoundsException(MessageFormat.format("数组大小为【】，index为【】，数组越界", this.count, index));
+        }
+
+        for (int i = index + 1; i < count; i++) {
+            data[i - 1] = data[i];
+        }
+
+        count--;
+        return true;
+    }
+
+    public void printAll() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(data[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Array array = new Array(10);
+        array.insert(0, 1);
+        array.insert(0, 2);
+        array.insert(0, 3);
+        array.insert(0, 4);
+        array.insert(0, 5);
+        array.insert(0, 6);
+        array.insert(0, 7);
+        array.insert(0, 8);
+        array.insert(0, 9);
+        array.insert(0, 10);
+        array.printAll();
+        System.out.println("=====");
+        array.delete(3);
+        array.printAll();
+    }
+}
---
+++ src/main/java/com/mright/algo/array/GenericArray.java
@@ -0,0 +1,236 @@
+package com.mright.algo.array;
+
+/**
+ * 泛型式通用的数组
+ *
+ * @author zhaochuanzhen
+ * @date 2022/4/24 10:52 上午
+ * @since 1.0
+ */
+public class GenericArray<T> {
+
+    /**
+     * 数据数组对象
+     */
+    private Object[] data;
+
+    /**
+     * 已存储的数据大小
+     */
+    private int size;
+
+    /**
+     * 指定容器大小的构造器
+     *
+     * @param capacity 初始大小
+     */
+    public GenericArray(int capacity) {
+        this.data = new Object[capacity];
+        this.size = 0;
+    }
+
+    /**
+     * 默认构造器，默认生成大小为10的
+     */
+    public GenericArray() {
+        this(10);
+    }
+
+    /**
+     * 获取当前容器大小
+     *
+     * @return 容器大小
+     */
+    public int getCapacity() {
+        return this.data.length;
+    }
+
+    /**
+     * 获取当前元素个数
+     *
+     * @return 当前元素个数
+     */
+    public int getCount() {
+        return this.size;
+    }
+
+    /**
+     * 当前是否为空
+     *
+     * @return 当前容器是否为空
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 修改 index 位置的元素
+    public void set(int index, T e) {
+        checkIndex(index);
+        data[index] = e;
+    }
+
+    // 获取对应 index 位置的元素
+    public T get(int index) {
+        checkIndex(index);
+        return (T) data[index];
+    }
+
+    // 查看数组是否包含元素e
+    public boolean contains(T e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 获取对应元素的下标, 未找到，返回 -1
+    public int find(T e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    // 在 index 位置，插入元素e, 时间复杂度 O(m+n)
+    public void add(int index, T e) {
+        checkIndexForAdd(index);
+        // 如果当前元素个数等于数组容量，则将数组扩容为原来的2倍
+        if (size == data.length) {
+            resize(2 * data.length);
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    // 向数组头插入元素
+    public void addFirst(T e) {
+        add(0, e);
+    }
+
+    // 向数组尾插入元素
+    public void addLast(T e) {
+        add(size, e);
+    }
+
+    // 删除 index 位置的元素，并返回
+    public T remove(int index) {
+        checkIndex(index);
+
+        T ret = (T) data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        data[size] = null;
+
+        // 缩容
+        if (size == data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+
+        return ret;
+    }
+
+    // 删除第一个元素
+    public T removeFirst() {
+        return remove(0);
+    }
+
+    // 删除末尾元素
+    public T removeLast() {
+        return remove(size - 1);
+    }
+
+    // 从数组中删除指定元素
+    public void removeElement(T e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("Array size = %d, capacity = %d \n", size, data.length));
+        builder.append('[');
+        for (int i = 0; i < size; i++) {
+            builder.append(data[i]);
+            if (i != size - 1) {
+                builder.append(", ");
+            }
+        }
+        builder.append(']');
+        return builder.toString();
+    }
+
+
+    // 扩容方法，时间复杂度 O(n)
+    private void resize(int capacity) {
+        T[] newData = (T[]) new Object[capacity];
+
+        for (int i = 0; i < size; i++) {
+            newData[i] = (T) data[i];
+        }
+        data = newData;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Add failed! Require index >=0 and index < size.");
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("remove failed! Require index >=0 and index <= size.");
+        }
+    }
+
+    public static void main(String[] args) {
+        GenericArray<User> users = new GenericArray<User>(10);
+        User user1 = new User("zs", 18);
+        User user2 = new User("ls", 15);
+
+        users.add(0, user1);
+//        users.add(1, user2);
+        System.out.println(users.toString());
+    }
+}
+
+class User {
+
+    private String name;
+
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
